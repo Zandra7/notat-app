8ca00bf070e90beb3e2708587e77422bca8a034c
Sandra Elise Eng <sandra.eng@live.no>
2024-04-30 11:18:37 +0200

    Lagt ti at man kan se eldre notater ved å klikke på dem Flyttet også alt inn i en if for å passe på at både 'title' og 'content' er fylt ut.

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 function saveNote() {
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const tags = document.getElementById('tags').value;
-
-    fetch('/add-note', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, content, tags })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Tømmer input-feltene
-        document.getElementById('title').value = '';
-        document.getElementById('content').value = '';
-        document.getElementById('tags').value = '';
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-
-    // Lager et nytt li-element og legger det til i listen
-    const ul = document.getElementById('prev-note');
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(title));
-    li.classList.add('note')
-    ul.appendChild(li);
+    if (document.getElementById('title').value === '' || document.getElementById('content').value === '') {
+        alert('Title and content must be filled out');
+        return;
+    } else {
+        const title = document.getElementById('title').value;
+        const content = document.getElementById('content').value;
+        const tags = document.getElementById('tags').value;
+        
+        fetch('/add-note', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title, content, tags })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Tømmer input-feltene
+            document.getElementById('title').value = '';
+            document.getElementById('content').value = '';
+            document.getElementById('tags').value = '';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    
+        // Lager et nytt li-element og legger det til i listen
+        const ul = document.getElementById('prev-notes');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(title));
+        li.classList.add('prev-note')
+        li.onclick = function() {
+            document.getElementById('title').value = title;
+            document.getElementById('content').value = content;
+            document.getElementById('tags').value = tags;
+        }
+    
+        ul.appendChild(li);
+    }
 }
  
