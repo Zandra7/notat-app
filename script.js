d130a3feb6a931102ba5eacf3f73ff5d7ba99cff
Sandra Elise Eng <sandra.eng@live.no>
2024-04-30 11:54:49 +0200

    Lagt til at man kan lagre notatet som en json fil når man klikker på export

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function saveNote() {
         const title = document.getElementById('title').value;
         const content = document.getElementById('content').value;
         const tags = document.getElementById('tags').value;
-        
+
         fetch('/add-note', {
             method: 'POST',
             headers: {
@@ -25,7 +25,7 @@ function saveNote() {
         .catch((error) => {
             console.error('Error:', error);
         });
-    
+
         // Lager et nytt li-element og legger det til i listen
         const ul = document.getElementById('prev-notes');
         const li = document.createElement('li');
@@ -36,8 +36,25 @@ function saveNote() {
             document.getElementById('content').value = content;
             document.getElementById('tags').value = tags;
         }
-    
+
         ul.appendChild(li);
     }
 }
- 
+
+// Lagre notatet som en JSON-fil
+function exportNote() {
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const tags = document.getElementById('tags').value;
+    const created = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
+    const changed = created;
+    const note = { title, content, tags, created, changed };
+    const data = JSON.stringify(note);
+    const filename = title + '.json';
+    const blob = new Blob([data], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href= url;
+    a.download = filename;
+    a.click();
+}
