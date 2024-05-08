00b25918e66b0a49d22d9fc6ed97759a55c2cd83
Sandra Elise Eng <sandra.eng@live.no>
2024-05-08 18:47:15 +0200

    Refakturert frontend koden for å bruke en Note klasse
    Flyttet også en del DOM kall nærmere DOM eventhandlerene (f.eks. ikke i DB insert/update funksjonene).

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let allNotes = [];
 
+class Note {
+    constructor(title, content, tags, created, changed) {
+        this.title = title;
+        this.content = content;
+        this.tags = tags;
+        if (created) {
+            this.created = created;
+        } else {
+            this.created = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
+        }
+        this.changed = changed || this.created;
+    }
+    // function to change the content of the note
+    updateNote(newContent, newTags) {
+        this.content = newContent;
+        this.tags = newTags;
+        this.changed = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
+    }
+}
+
 function addTagOnEnter(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
@@ -8,6 +28,7 @@ function addTagOnEnter(event) {
     }
 }
 
+// Oppdaterer det som er synlig på nettsiden
 function addTag() {
     const tagInput = document.getElementById('tags');
     const currentTags = document.getElementById('current-tags');
@@ -32,124 +53,105 @@ function addTag() {
     }
 }
 
-function insertNote() {
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    // Lager en string av tags separert med semikolon
-    const tags = Array.from(document.getElementById('current-tags').children).map(li => li.getAttribute('data-tag')).join(';');
-    console.log('tags:', tags);
+function insertNoteInDB(note) {
     fetch('/insert-note', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ title, content, tags })
+        body: JSON.stringify(note)
     })
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        // Tømmer input-feltene og tag-listen
-        document.getElementById('title').value = '';
-        document.getElementById('content').value = '';
-        document.getElementById('tags').value = '';
-        document.getElementById('current-tags').innerHTML = '';
-        document.getElementById('date').style.display = 'none';
-        // Legger til notatet i allNotes for å slippe å hente ut fra databasen
-       let now = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
-       allNotes.push({ 
-            Title: title, 
-            Content: content, 
-            Tags: tags, 
-            Created: now, 
-            Changed: now});
     })
     .catch((error) => {
         console.error('Error:', error);
     });
-    // Lager et nytt li-element og legger det til i listen
-    const ul = document.getElementById('prev-notes');
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(title));
-    li.classList.add('prev-note')
-    ul.appendChild(li);
-    // Klikke på li for å vise notatet i input-feltene fra databasen
-    li.onclick = function() {
-        const note = allNotes.find(note => note.Title === title);
-        console.log('Note:', note);
-        if (note) {
-            showNote(note);
-        } else {
-            console.log('No note found with title:', title);
-        }
-    }
 }
 
-function updateNote() {
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const tags = Array.from(document.getElementById('current-tags').children).map(li => li.getAttribute('data-tag')).join(';');
+function updateNoteInDB(note) {
     fetch('/update-note', {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ title, content, tags })
+        body: JSON.stringify(note)
     })
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        // Tømmer input-feltene og tag-listen
-        document.getElementById('title').value = '';
-        document.getElementById('content').value = '';
-        document.getElementById('tags').value = '';
-        document.getElementById('current-tags').innerHTML = '';
-        document.getElementById('date').style.display = 'none';
-        // Oppdaterer notatet i allNotes
-        const note = allNotes.find(note => note.Title === title);
-        if (note) {
-            let now = new Date().toLocaleString('no-NB', { timeZone: 'Europe/Oslo' });
-            note.Content = content;
-            note.Tags = tags;
-            note.Changed = now;
-        } else {
-            console.log('No note found with title:', title);
-        }
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 }
 
+function clearInputFields() {
+    // Tømmer input-feltene og tag-listen
+    document.getElementById('title').value = '';
+    document.getElementById('content').value = '';
+    document.getElementById('tags').value = '';
+    document.getElementById('current-tags').innerHTML = '';
+    document.getElementById('date').style.display = 'none';
+}
+
+// denne funksjonen kalles fra GUI når brukeren ønsker å lagre et nytt notat eller eksisterende notat
 function saveNote() {
     if (document.getElementById('title').value === '' || document.getElementById('content').value === '') {
         alert('Title and content must be filled out');
         return;
     } else {
-        if (allNotes.some(note => note.Title === document.getElementById('title').value)) {
+        const title = document.getElementById('title').value;
+        const content = document.getElementById('content').value;
+        const tags = Array.from(document.getElementById('current-tags').children).map(li => li.getAttribute('data-tag')).join(';');
+
+        existingNote = allNotes.find(note => note.title === title);
+        if (existingNote) {
             console.log('Note already exists, updating');
-            updateNote();
+            existingNote.updateNote(content, tags);
+            updateNoteInDB(existingNote);
         } else {
             console.log('Note does not exist, inserting');
-            insertNote(); 
+            const newNote = new Note(title, content, tags);
+            insertNoteInDB(newNote);
+            allNotes.push(newNote); 
+            // Lager et nytt li-element og legger det til i listen
+            const ul = document.getElementById('prev-notes');
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(title));
+            li.classList.add('prev-note')
+            ul.appendChild(li);
+            // Klikke på li for å vise notatet i input-feltene fra databasen
+            li.onclick = function() {
+                const note = allNotes.find(note => note.title === title);
+                console.log('Note:', note);
+                if (note) {
+                    showNote(note);
+                } else {
+                    console.log('No note found with title:', title);
+                }
+            }
         }
+        clearInputFields();
     }
 }
 
+// Viser notatet i input-feltene
 function showNote(note) {
-    document.getElementById('title').value = note.Title;
-    document.getElementById('content').value = note.Content;
-
+    document.getElementById('title').value = note.title;
+    document.getElementById('content').value = note.content;
     document.getElementById('date').style.display = 'block';
-    document.getElementById('created').innerHTML = note.Created;
-    document.getElementById('modified').innerHTML = note.Changed;
+    document.getElementById('created').innerHTML = note.created;
+    document.getElementById('modified').innerHTML = note.changed;
 
-    if (note.Created === note.Changed) {
+    if (note.created === note.changed) {
         document.getElementById('modified-info').style.display = 'none';
     } else {
         document.getElementById('modified-info').style.display = 'block';
     }
 
-    const tags = String(note.Tags).split(';');
+    const tags = String(note.tags).split(';');
     const currentTags = document.getElementById('current-tags');
     currentTags.innerHTML = '';
     tags.forEach(tag => {
@@ -170,7 +172,7 @@ function showNote(note) {
 // Lagre notatet som en JSON-fil med tags i 'current-tags'
 function exportNote() {
     const title = document.getElementById('title').value;
-    const note = allNotes.find(note => note.Title === title);
+    const note = allNotes.find(note => note.title === title);
     if (note) {
         const data = JSON.stringify(note);
         const blob = new Blob([data], { type: 'application/json' });
@@ -197,8 +199,8 @@ function importNote() {
         reader.onload = function(event) {
             importedNote = JSON.parse(event.target.result);
 
-            if (allNotes.some(note => note.Title === importedNote.Title)) {
-                alert('Note with title, ' + importedNote.Title + ', already exists');
+            if (allNotes.some(note => note.title === importedNote.title)) {
+                alert('Note with title, ' + importedNote.title + ', already exists');
                 return;
             }
 
@@ -207,7 +209,7 @@ function importNote() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({title:importedNote.Title, content:importedNote.Content, tags:importedNote.Tags, created:importedNote.Created, changed:importedNote.Changed})
+                body: JSON.stringify(importedNote)
             })
             .then(response => response.json())
             .then(data => {
@@ -217,7 +219,7 @@ function importNote() {
                 // Lager et nytt li-element og legger det til i listen
                 const ul = document.getElementById('prev-notes');
                 const li = document.createElement('li');
-                li.appendChild(document.createTextNode(importedNote.Title));
+                li.appendChild(document.createTextNode(importedNote.title));
                 li.classList.add('prev-note')
                 ul.appendChild(li);
                 // Klikke på li for å vise notatet i input-feltene fra databasen
@@ -250,13 +252,13 @@ function showNotes(filter) {
     allNotes.forEach(note => {
         let matchCount = 0;
         for (let i = 0; i < filter.length; i++) {
-            if (note.Tags.includes(filter[i])) {
+            if (String(note.tags).includes(filter[i])) {
                 matchCount++;
             }
         }
         if (matchCount === filter.length) {
             const li = document.createElement('li');
-            li.appendChild(document.createTextNode(note.Title));
+            li.appendChild(document.createTextNode(note.title));
             li.classList.add('prev-note')
             ul.appendChild(li);
             li.onclick = showNote.bind(null, note);
@@ -274,7 +276,10 @@ fetch('/get-notes', {
 })
 .then(response => response.json())
 .then(data => {
-    allNotes = data;
+    // Legg til hvert notat mottatt fra databasen inn i allNotes som Note-objekter
+    data.map(note => {
+        allNotes.push(new Note(note.Title, note.Content, note.Tags, note.Created, note.Changed));
+    })
     console.log('All notes:', allNotes);
     showNotes('');
 });
